28d38c8508fccdadee70f764a0bc1ac35291a76b
hamza1030 <hnougba@ulb.ac.be>
2016-12-26 21:50:22 +0100

    a

--- ostream/ostream13.cpp
+++ ostream/ostream13.cpp
@@ -5,24 +5,21 @@ using namespace std;
 OStream13::OStream13() {}
 
 void OStream13::create(char *filename) {
-    int ofile = _open(filename, _O_WRONLY);
+  int ofile = _open(filename, _O_WRONLY);
 
-    if (ofile == -1) {
-        cout << "Error";
-        exit(1);
-    }
+  if (ofile == -1) {
+    cout << "Error";
+    exit(1);
+  }
 
-    ofile13 = ofile;
+  ofile13 = ofile;
 }
 
 void OStream13::write(vector<int> elems) {
-    // int *a = &elems;
-    cout << "hey" << elems.size() << endl;
-    //marche pas...
 
-    _write(ofile13, &elems, elems.size() * 4);
+  cout << "hey" << elems[0] << endl;
+  // marche pas...
+  _write(ofile13, &elem, 4);
 }
 
-void OStream13::close() {
-    _close(ofile13);
-}
+void OStream13::close() { _close(ofile13); }

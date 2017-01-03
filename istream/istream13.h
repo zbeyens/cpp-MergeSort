a619c42dd901377ef84676bc3ddf5893de39d7fc
hamza1030 <hnougba@ulb.ac.be>
2017-01-03 13:39:41 +0100

    a part of part 3

--- istream/istream13.h
+++ istream/istream13.h
@@ -2,23 +2,25 @@
 #include <io.h>
 #include <iostream>
 #include <stdlib.h>
-#include <vector>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <vector>
 
 class IStream13 {
 private:
-int offset;
-int length;
-int ofile13;
-int B;
-int Bcurrent;
-char *buffer;
+  int offset;
+  int length;
+  int ofile13;
+  int B;
+  int Bcurrent;
+  char *buffer;
 
 public:
-IStream13(int b);
-IStream13();
-void open(char *filename);
-std::vector<int> read_next();
-bool end_of_stream();
+  IStream13(int b);
+  IStream13();
+  void open(char *filename);
+  std::vector<int> read_next();
+  bool end_of_stream();
+  int get_length();
+  void set_pointer(int place);
 };
--- merge.cpp
+++ merge.cpp
@@ -4,19 +4,19 @@ void merge_sort(vector<vector<int>> listOfStreams, vector<int> *output) {
   priority_queue<Element, vector<Element>, Comp> min_heap;
   for (unsigned int i = 0; i < listOfStreams.size(); i++) {
     if (!listOfStreams[i].empty()) {
-		
-		Element elem;
-		elem.x = i;
-		elem.value = listOfStreams[i][0];
-		cout<<"valeur :"<<elem.value<<endl;
-		min_heap.push(elem);
+
+      Element elem;
+      elem.x = i;
+      elem.value = listOfStreams[i][0];
+      cout << "value :" << elem.value << endl;
+      min_heap.push(elem);
     }
   }
   if (!min_heap.empty()) {
-	Element min = min_heap.top();
-	int value = min.value;
-	cout<<"minimum: "<< value<< endl;
-	output->push_back(value);
+    Element min = min_heap.top();
+    int value = min.value;
+    cout << "minimum: " << value << endl;
+    output->push_back(value);
     listOfStreams[min.x].erase(listOfStreams[min.x].begin());
     merge_sort(listOfStreams, output);
   }

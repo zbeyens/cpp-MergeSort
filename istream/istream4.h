97bf1c7b98bc04b6d33bbee86835bfd6f87da623
agarciad <antonio.garcia.diaz@ulb.ac.be>
2017-01-03 13:54:03 +0100

    Changes to detect source of error in version 4 (related to offset values different from 0).

--- istream/istream4.h
+++ istream/istream4.h
@@ -9,19 +9,19 @@ using namespace std;
 using namespace boost::iostreams;
 
 class IStream4 {
-private:
-// char *ipath;
-int offset;
-int length;
-mapped_file_source ifile4;
-mapped_file_params param;
-int B;
-int Bcurrent;
-int *buffer;
+    private:
+    // char *ipath;
+    int offs; //Mapped portion offset
+    int len; //Mapped portion length
+    mapped_file_source ifile4;
+    mapped_file_params param;
+    int B;
+    int Bcurrent;
+    int *buffer;
 
-public:
-IStream4(int b);
-void open(char *filename);
-std::vector<int> read_next();
-bool end_of_stream();
+    public:
+    IStream4(int b);
+    void open(char *filename);
+    std::vector<int> read_next();
+    bool end_of_stream();
 };
